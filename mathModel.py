fc236c79d746bfa3dff00361e465ab461e4de02e
kennynguyen1603 <kennynguyen1631984@gmail.com>
2024-04-18 11:55:11 +0700

    update mathModel by python

---
+++ mathModel.py
@@ -0,0 +1,66 @@
+from ortools.constraint_solver import pywrapcp, routing_enums_pb2
+
+def load_distance_matrix(file_path):
+    """Load the distance matrix from a file."""
+    distance_matrix = []
+    with open(file_path, 'r') as file:
+        for line in file:
+            row = list(map(float, line.split()))
+            distance_matrix.append(row)
+    return distance_matrix
+
+
+def print_solution(manager, routing, solution):
+    """Prints solution on console."""
+    print('Objective: {} miles'.format(solution.ObjectiveValue()))
+    index = routing.Start(0)
+    plan_output = 'Route:\n'
+    route_distance = 0
+    while not routing.IsEnd(index):
+        plan_output += ' {} ->'.format(manager.IndexToNode(index))
+        previous_index = index
+        index = solution.Value(routing.NextVar(index))
+        route_distance += routing.GetArcCostForVehicle(previous_index, index, 0)
+    plan_output += ' {}\n'.format(manager.IndexToNode(index))
+    print(plan_output)
+    print('Route distance: {} miles'.format(route_distance))
+
+def main():
+    # Instantiate the data problem.
+    file_path = './TSP/cities17.txt'
+    distance_matrix = load_distance_matrix(file_path)
+
+    # Create the routing index manager.
+    manager = pywrapcp.RoutingIndexManager(len(distance_matrix), 1, 0)
+
+    # Create Routing Model.
+    routing = pywrapcp.RoutingModel(manager)
+
+    # Create and register a transit callback.
+    def distance_callback(from_index, to_index):
+        # Convert from routing variable Index to distance matrix NodeIndex.
+        from_node = manager.IndexToNode(from_index)
+        to_node = manager.IndexToNode(to_index)
+        return distance_matrix[from_node][to_node]
+
+    transit_callback_index = routing.RegisterTransitCallback(distance_callback)
+
+    # Define cost of each arc.
+    routing.SetArcCostEvaluatorOfAllVehicles(transit_callback_index)
+
+    # Setting first solution heuristic.
+    search_parameters = pywrapcp.DefaultRoutingSearchParameters()
+    search_parameters.first_solution_strategy = (
+        routing_enums_pb2.FirstSolutionStrategy.PATH_CHEAPEST_ARC)
+
+    # Solve the problem.
+    solution = routing.SolveWithParameters(search_parameters)
+
+    # Print solution on console.
+    if solution:
+        print_solution(manager, routing, solution)
+    else:
+        print('No solution found!')
+
+if __name__ == '__main__':
+    main()
